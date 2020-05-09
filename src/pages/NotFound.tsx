ad8f5f49b951118074f72d2fd95c5d488422d605
Kevin Detter <kevin@matthiashamann.work>
2020-05-09 14:50:50 +0200

    -added content to NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const NotFound = () => {
   return (
     <>
-      <h1>Not Found</h1>
+      <header>
+        <h1>Not Found</h1>
+        <p>
+          The requested page could not be found. <a href="/" title="Go back to start">Go back to start</a>.
+        </p>
+      </header>
     </>
   );
 }
