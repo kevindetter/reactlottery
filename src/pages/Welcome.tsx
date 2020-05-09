5e13272b41408dd96ed1e2937044ef0edf9f6b64
Kevin Detter <kevin@matthiashamann.work>
2020-05-09 14:48:54 +0200

    -added content to Welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Welcome = () => {
   return (
     <>
-      <h1>Welcome</h1>
+      <header>
+        <h1>Welcome to React Lottery</h1>
+        <p>
+          This is a simple page to present latest <a href="/results" title="Go to EuroJackpot results">EuroJackpot drawing results</a>.
+        </p>
+      </header>
     </>
   );
 }
