00d7d19d7dc290ccc26e269d3ae8cca97d9c7591
Kevin Detter <kevin@matthiashamann.work>
2020-05-09 12:18:11 +0200

    -added page header and footer

--- src/components/PageWrap.tsx
+++ src/components/PageWrap.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
+import { NavLink } from 'react-router-dom';
+
 type PageWrapProps = {
   children: any
 }
 
 const PageWrap = ({ children }: PageWrapProps) => {
   return (
-    <main>{children}</main>
+    <>
+      <header className="page-header">
+        <nav>
+          <NavLink to="/" title="Go to start page">Home</NavLink>
+          <NavLink to="/results" title="See Eurojackpot results">Results</NavLink>
+        </nav>
+      </header>
+
+      <main>{children}</main>
+      
+      <footer className="page-footer">
+        <span>&copy; {new Date().getFullYear()} React Lottery</span>
+        <nav>
+          <NavLink to="/contact" title="Go to contact page">Contact</NavLink>
+          <NavLink to="/terms" title="Read our terms and conditions">Terms &amp; Conditions</NavLink>
+          <NavLink to="/imprint" title="Go to imprint page">Imprint</NavLink>
+        </nav>
+      </footer>
+    </>
   );
 }
 
