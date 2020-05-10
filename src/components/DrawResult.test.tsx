323f0af8794a4c71fdb3c9b3d7df88c23eabbed2
Kevin Detter <kevin@matthiashamann.work>
2020-05-10 14:33:55 +0200

    -added 'smoke tests' to project

--- src/components/DrawResult.test.tsx
+++ src/components/DrawResult.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import DrawResult from './DrawResult';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<DrawResult />, div);
 });
--- src/components/PageHeader.test.tsx
+++ src/components/PageHeader.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import PageHeader from './PageHeader';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<PageHeader />, div);
 });
